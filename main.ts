c9e58df72d029ec05b6e1e68ba7f9ff85d6895ec
kuchbhimingwal <77461374+kuchbhimingwal@users.noreply.github.com>
2021-03-12 05:39:20 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,15 @@
-
-basic.showString("Shake to count")
-basic.showString("B to clear")
-
-let shake = 0
-input.onGesture(Gesture.Shake, function () {
-    shake += 1
-})
-basic.forever(function () {
-    basic.showNumber(shake)
+input.onButtonPressed(Button.A, function () {
+    control.reset()
 })
 input.onButtonPressed(Button.B, function () {
     shake = 0
 })
-input.onButtonPressed(Button.A, function(){
-    control.reset()
+input.onGesture(Gesture.Shake, function () {
+    shake += 1
+})
+let shake = 0
+basic.showString("Shake to count")
+basic.showString("B to clear")
+basic.forever(function () {
+    basic.showNumber(shake)
 })
